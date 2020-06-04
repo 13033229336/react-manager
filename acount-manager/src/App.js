a3744da10bec6f65c274ff8906054053393fcd57
13033229336 <874609947@qq.com>
2020-06-04 13:40:28 +0800

    viewtab

--- acount-manager/src/App.js
+++ acount-manager/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import PriceList from './Components/PriceList'
+import ViewTab from './Components/ViewTab'
+import {LIST_VIEW,CHART_VIEW} from './utility'
 const items=[
   {
     "id":1,
@@ -10,7 +12,7 @@ const items=[
     "category":{
       "id":"1",
       "name":"旅行",
-      "type":"outcome",
+      "iconName":"ios-plane",
     } 
   },
   {
@@ -21,18 +23,22 @@ const items=[
     "category":{
       "id":"1",
       "name":"旅行",
-      "type":"outcome",
+      "iconName":"ios-plane",
     } 
   },
 ]
 function App() {
   return (
     <div className="App">
-      <PriceList 
+      <ViewTab
+         activeTab={LIST_VIEW}
+         onTabChange={(view)=>{console.log(view)}}
+      />
+      {/* <PriceList 
         items={items}
         onModifyItem={(item)=>{alert(item.id)}}
         onDeleteItem={(item)=>{alert(item.id)}}
-      />
+      /> */}
     </div>
   );
 }
--- acount-manager/src/Components/PriceList.js
+++ acount-manager/src/Components/PriceList.js
@@ -1,22 +1,56 @@
 import React from 'react'
-
+import Ionicon from 'react-ionicons'
+import PropType from 'prop-types'
 const PriceList=({items , onModifyItem,onDeleteItem})=>{
     return(
         <ul className="list-group list-group-flush">
             {
                 items.map((item)=>(
                     <li className="list-group-item d-flex justify-content-netween align-items-center" key={item.id}>
-                       <span className="col-1 badge badge-primary">{item.category.name}</span>
-                       <span className="col-5">{item.title}</span>
-                       <span className="col-2 font-weight-bold">{(item.category.type==='income')? '+':'-'}{item.price}元</span>
-                       <span className="col-2">{item.date}</span>
-                       <button className="col-1 btn btn-primary" onClick={()=>{onModifyItem(item)}}>编辑</button>
-                       <button className="col-1 btn btn-danger" onClick={()=>{onDeleteItem(item)}}>删除</button>
+                        <Ionicon
+                            className="rounded-circle" 
+                            fontSize="30px" 
+                            style={{backgroundColor:'#007bff',padding:'5px'}} color={'#fff'} 
+                            icon={item.category.iconName}
+                        />
+                        <span className="col-5">{item.title}</span>
+                        <span className="col-2 font-weight-bold">
+                            {(item.category.type==='income')? '+':'-'}
+                            {item.price}元
+                        </span>
+                        <span className="col-2">{item.date}</span>
+                        <a className="col-1" onClick={()=>{onModifyItem(item)}}>
+                           <Ionicon 
+                               className="rounded-circle" 
+                               fontSize="30px" 
+                               style={{backgroundColor:'#28a745',padding:'5px'}} 
+                               color={'#fff'} 
+                               icon='ios-create-outline'
+                            />
+                        </a>
+                       <a className="col-1" onClick={()=>{onDeleteItem(item)}}>
+                            <Ionicon 
+                               className="rounded-circle" 
+                               fontSize="30px" 
+                               style={{backgroundColor:'#dc3545',padding:'5px'}} 
+                               color={'#fff'} 
+                               icon='ios-close'
+                            />
+                       </a>
                     </li>
                 ))
             }
         </ul>
     )
 }
-
+//类型检查
+PriceList.propType={
+    items:PropType.array.isRequired,
+    onModifyItem:PropType.func.isRequired,
+    onDeleteItem:PropType.func.isRequired,
+}
+PriceList.defaultProps={
+    onModifyItem:()=>{},
+    onDeleteItem:()=>{}
+}
 export default PriceList

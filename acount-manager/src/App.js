0fcaa9dd03d31a1c7ebf7740db99943689abcf55
13033229336 <874609947@qq.com>
2020-06-04 10:48:45 +0800

    价格列表组件

---
+++ acount-manager/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import PriceList from './Components/PriceList'
+const items=[
+  {
+    "id":1,
+    "title":"去南昌旅行",
+    "price":200,
+    "date":"2020-4-5",
+    "category":{
+      "id":"1",
+      "name":"旅行",
+      "type":"outcome",
+    } 
+  },
+  {
+    "id":2,
+    "title":"去南昌旅行",
+    "price":200,
+    "date":"2020-4-5",
+    "category":{
+      "id":"1",
+      "name":"旅行",
+      "type":"outcome",
+    } 
+  },
+]
+function App() {
+  return (
+    <div className="App">
+      <PriceList 
+        items={items}
+        onModifyItem={(item)=>{alert(item.id)}}
+        onDeleteItem={(item)=>{alert(item.id)}}
+      />
+    </div>
+  );
+}
+
+export default App;
